45bcca36c09530a908f850de51575b333578163a
dangrt5 <randy.dang05@gmail.com>
2018-09-27 18:40:16 -0700

    mern stack setup

---
+++ client/src/components/test.js
@@ -0,0 +1,27 @@
+import React, {Component} from "react";
+import axios from "axios";
+
+class Test extends Component {
+  async componentDidMount() {
+    const user = {
+      email: "test@mail.com",
+      password: "asdffjsdaklqw1"
+    }
+
+    const resp = await axios.post("/api/login", user);
+
+    console.log("Login response", resp);
+
+    // const resp = await axios.get("/api/test");
+    // console.log("Test Response", resp);
+    //
+    // const user = await axios.get("/api/user");
+    // console.log("User Data:", user);
+  }
+
+  render() {
+    return <h1>API Testing Component</h1>
+  };
+};
+
+export default Test;
--- index.js
+++ index.js
@@ -1,10 +1,43 @@
 const express = require("express");
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 9000;
+const {resolve} = require("path");
+const cors = require("cors");
 
 const app = express();
 
-app.get("/", (req, response) => {
-  response.send("<h1>hi</h1>");
+app.use(cors());
+
+app.use(express.static(resolve(__dirname, "client", "dist")));
+app.use(express.json());
+
+app.post("/api/login", (req, res) => {
+  console.log("Post Data:", req.body);
+
+  res.send({
+    success: true,
+    message: "You are now logged in",
+    receivedData: req.body
+  })
+})
+
+app.get("/api/test", (req, response) => {
+  response.send({
+    success: true,
+    message: "The API is working",
+    date: new Date().toDateString()
+  });
+});
+
+app.get("/api/user", (req, response) => {
+  response.send({
+    name: "Kathy Simmons",
+    age: 38,
+    email: "therealkathy@mail.com"
+  })
+})
+
+app.get("*", (req, response) => {
+  response.sendFile(resolve(__dirname, "client", "dist", "index.html"));
 })
 
 app.listen(PORT, () => {
